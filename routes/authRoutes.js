59a603907010dc334ce9c2ce9c937d853ae60caa
Vera Kippel <veki2400@student.miun.se>
2025-05-20 16:58:15 +0200

    lägger paket i variabler

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(cors());
