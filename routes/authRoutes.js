058a3dd6e9693bf406fe0dcb26fa0e68eb6ab3fa
Vera Kippel <veki2400@student.miun.se>
2025-05-20 17:00:08 +0200

    lägger in routes och start för applikation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,3 +8,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
+
+//Routes
+app.use("/api", authRoutes);
+
+//Starta appikation
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})
