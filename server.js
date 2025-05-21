a2cd0688449c18d122f62c5a687f3107bc0bda9d
Vera Kippel <veki2400@student.miun.se>
2025-05-21 15:29:42 +0200

    tar bort get-route

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //Routes
-
-app.get("/", (req, res) => {
-    res.json({message: "hello"});
-})
-
 app.use("/api", authRoutes);
 
 //Starta appikation
